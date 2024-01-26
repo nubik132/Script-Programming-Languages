b667cf8c97faffc4ef7bac7fc72461339e755bf7
nubik132 <mister.nubik@list.ru>
2024-01-26 15:44:41 +0300

    feat: create table on Basket page

--- Лаб. 1 (сем. 2)/basket_script.js
+++ Лаб. 1 (сем. 2)/basket_script.js
@@ -8,27 +8,28 @@ function countProducts() {
     let obj = {}
     for (const product of basket.products) {
         if (product.name in obj) {
-            obj[product.name]
+            obj[product.name]++;
+        } else {
+            obj[product.name] = 1;
         }
     }
+    return obj;
 }
-
-for (const category of basket.products) {
-    categoryNameHtml.innerText = category.name
-    categoryHtml.appendChild(categoryNameHtml);
-    table.appendChild(categoryHtml);
-
-    for (const product of category.products) {
+function init(products) {
+    for (const key in products) {
         let productRowHtml = document.createElement("tr");
         productRowHtml.classList.add("product");
 
         let productHtml = document.createElement("td");
-        productHtml.innerText = product.name;
+        productHtml.innerText = key;
 
         productRowHtml.appendChild(productHtml);
-        let td = decorateHtml(createPlusButton(product), "td");
-        productRowHtml.appendChild(td);
-        
+
+        let count = document.createElement("td");
+        count.innerText = products[key];
+        productRowHtml.appendChild(count);
+
+        productRowHtml.appendChild(decorateHtml(createPlusButton(product), "td"));
         productRowHtml.appendChild(decorateHtml(createMinusButton(product), "td"));
 
         table.appendChild(productRowHtml);

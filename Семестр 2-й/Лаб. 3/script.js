37fe292f39344433713f7e5fd34a0c7c94c6543c
nubik132 <mister.nubik@list.ru>
2024-02-19 16:24:28 +0300

    fix: half task

--- Семестр 2-й/Лаб. 3/script.js
+++ Семестр 2-й/Лаб. 3/script.js
@@ -1,4 +1,3 @@
-
 function explainArray() {
     console.log("Массив (Array) - это упорядоченная коллекция элементов.");
     console.log("Элементы в массиве могут быть любого типа данных, включая числа, строки, объекты и т.д.");
@@ -12,8 +11,8 @@ function visualizeArray() {
 // Функция для демонстрации работы с массивом
 function arrayOperations() {
     let myArray = [];
-
     // Человек вводит данные в массив
+
     let number = 23;
     myArray.push(number);
 
@@ -34,8 +33,51 @@ function multiDimensionalArray() {
 
     console.log("Пример многомерного массива (матрицы):", matrix);
 }
+let firstArray = [1, 2, 3, 4, 5];
+function addElement() {
+    let element = prompt("Напишите элемент");
+    firstArray.push(element);
+    updateFirstArray();
 
+}
+
+function deleteElement() {
+    firstArray.pop();
+    updateFirstArray();
+}
+
+function updateFirstArray(array = firstArray) {
+    let firstArrayHtml = document.getElementById("first-array");
+    firstArrayHtml.innerText = "[" + array.join(", ") + "]";
+}
+
+let addingArray = [];
+
+function addToAddingArray() {
+    let input = document.getElementById("input");
+    let addingArrayHtml = document.getElementById("adding-array");
+    addingArray = input.value.split("\n").map(str => str.split(", "));
+    addingArrayHtml.innerText = addingArray.map(array => "[" + array.join(", ") + "]").join("\n");
+}
+
+function transposeArray(array, arrayLength) {
+    var newArray = [];
+    for (var i = 0; i < array.length; i++) {
+        newArray.push([]);
+    };
+
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < arrayLength; j++) {
+            newArray[j].push(array[i][j]);
+        };
+    };
+
+    return newArray;
+}
+
+updateFirstArray(firstArray);
 explainArray();
 visualizeArray();
 arrayOperations();
 multiDimensionalArray();
+

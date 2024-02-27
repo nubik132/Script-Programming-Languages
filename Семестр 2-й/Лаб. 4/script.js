7dfc4b9a102f504946b9bdf78167a7c93bfb9b8c
nubik132 <mister.nubik@list.ru>
2024-02-27 10:52:13 +0300

    feat: complte task 4

--- Семестр 2-й/Лаб. 4/script.js
+++ Семестр 2-й/Лаб. 4/script.js
@@ -1,19 +1,29 @@
-// Функция для вывода текущей даты и времени с обновлением каждую секунду
+const currentTime = document.getElementById("current-time")
+const secondsFrom2009 = document.getElementById("seconds-from-2009")
+const difference = document.getElementById("difference")
+const dayAndMonth = document.getElementById("day-and-month")
+
+const firstDif = document.getElementById("first-dif")
+const secondDif = document.getElementById("second-dif")
+const dayAndMonthInput = document.getElementById("day-and-month-input")
+
+firstDif.addEventListener('change', dayAndMonthNameFromDateEvent);
+secondDif.addEventListener('change', dayAndMonthNameFromDateEvent);
+dayAndMonthInput.addEventListener('change', () => { dayAndMonthNameFromDate(dayAndMonthInput.value) })
+
+
 function displayCurrentDateTime() {
-        const currentDate = new Date();
-        const formattedDateTime = currentDate.toLocaleString('ru-RU', { second: 'numeric' });
-        console.log(`Текущая дата и время: ${formattedDateTime}`);
+    const currentDate = new Date();
+    currentTime.textContent = currentDate.toLocaleString('ru-RU');
 }
 
-// Функция для подсчета секунд с определенной даты
 function secondsSinceSpecificDate() {
     const specificDate = new Date('2009-04-17T00:00:00');
-        const currentDate = new Date();
-        const secondsDifference = Math.floor((currentDate - specificDate) / 1000);
-        console.log(`Прошло секунд с 17.04.2009: ${secondsDifference}`);
+    const currentDate = new Date();
+    const secondsDifference = Math.floor((currentDate - specificDate) / 1000);
+    secondsFrom2009.textContent = secondsDifference;
 }
 
-// Функция для подсчета дней и недель между двумя датами
 function daysAndWeeksBetweenDates(date1, date2) {
     const startDate = new Date(date1);
     const endDate = new Date(date2);
@@ -22,19 +32,23 @@ function daysAndWeeksBetweenDates(date1, date2) {
     const daysDifference = Math.floor(millisecondsDifference / (1000 * 60 * 60 * 24));
     const weeksDifference = Math.floor(daysDifference / 7);
 
-    console.log(`Между ${date1} и ${date2}: ${daysDifference} дней, ${weeksDifference} недель`);
+    difference.textContent = `Между ${date1} и ${date2}: ${daysDifference} дней, ${weeksDifference} недель`;
 }
 
-// Функция для возвращения названия дня и месяца по дате
+function dayAndMonthNameFromDateEvent() {
+    daysAndWeeksBetweenDates(firstDif.value, secondDif.value);
+}
+
 function dayAndMonthNameFromDate(dateString) {
     const date = new Date(dateString);
     const dayName = date.toLocaleDateString('ru-RU', { weekday: 'long' });
     const monthName = date.toLocaleDateString('ru-RU', { month: 'long' });
 
-    console.log(`День: ${dayName}, Месяц: ${monthName}`);
+    dayAndMonth.textContent = `День: ${dayName}, Месяц: ${monthName}`;
 }
-
-displayCurrentDateTime();
-secondsSinceSpecificDate();
+setInterval(() => {
+    displayCurrentDateTime();
+    secondsSinceSpecificDate();
+}, 1000);
 daysAndWeeksBetweenDates('2004-03-18', '2017-07-28');
 dayAndMonthNameFromDate('2024-02-18');

9adda544f4ca794a01e64eefd5adf26d309b662a
nubik132 <118123709+nubik132@users.noreply.github.com>
2024-02-27 13:14:22 +0000

    feat: news

--- Семестр 2-й/Лаб. 10/script.js
+++ Семестр 2-й/Лаб. 10/script.js
@@ -1,23 +1,19 @@
-let json = async()
-console.log(json);
-for (const art of json.articles) {
-    let h2 = document.createElement("h2")
-    h2.innerText = art.title
-    let description = document.createElement("p")
-    description.innerText = art.description
-    let content = document.createElement("p")
-    content.innerText = art.content
-    let news = document.createElement("div")
-    news.appendChild(h2)
-    news.appendChild(description)
-    news.appendChild(content)
-    document.body.appendChild(news)
-}
+async();
 
 async function async(){
     let response = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2024-01-27&sortBy=publishedAt&apiKey=844d5795b1734116bcae513b4dfbe504");
-    console.log("респонс");
-    console.log(response);
-    console.log("жсон");
-    return await response.json();
+    let json = await response.json();
+    for (const art of json.articles) {
+        let h2 = document.createElement("h2")
+        h2.innerHTML = art.title
+        let description = document.createElement("p")
+        description.innerHTML = art.description
+        let content = document.createElement("p")
+        content.innerHTML = art.content
+        let news = document.createElement("div")
+        news.appendChild(h2)
+        news.appendChild(description)
+        news.appendChild(content)
+        document.body.appendChild(news)
+    }
 }
